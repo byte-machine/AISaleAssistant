2c6a4b66a9f568acb43eb0de0dba86a95e9c9993
DOKUZUSAMA <dkobdabaev@mail.ru>
2025-02-11 17:58:39 +0500

    get chats endpoint

--- api/infrastructure/rest/chat.go
+++ api/infrastructure/rest/chat.go
@@ -12,6 +12,7 @@ func ChatRoutes(router *gin.Engine, settings config.Settings) {
 	{
 		productGroup.POST("/send_message", ai_controllers.SendMessage)
 		productGroup.POST("/whatsapp_webhook", ai_controllers.WhatsappWebhook)
+		productGroup.GET("/get_chats", chat_controllers.GetChats)
 		productGroup.POST("/get_chat", chat_controllers.GetChatHistory)
 	}
 }
