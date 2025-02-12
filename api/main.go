3c561f7b5b5aea7a0cd559453cd952b2d855c09d
DOKUZUSAMA <dkobdabaev@mail.ru>
2025-02-12 17:16:52 +0500

    determinate client

--- api/main.go
+++ api/main.go
@@ -19,8 +19,6 @@ func RouterStart(settings config.Settings) {
 	}))
 
 	rest.ChatRoutes(r, settings)
-	rest.AdminRoutes(r, settings)
-	rest.ParsingRoutes(r, settings)
 
 	err := r.Run(":8081")
 	if err != nil {
